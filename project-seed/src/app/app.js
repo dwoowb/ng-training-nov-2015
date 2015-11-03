e911eda67282866c908fa34a8a6db360359ae7d3
joelhooks <joelhooks@gmail.com>
2015-11-03 10:36:59 -0800

    add the user-list module

--- project-seed/src/app/app.js
+++ project-seed/src/app/app.js
@@ -1,15 +1,10 @@
 angular.module('project-seed', [
+  'ps.user-list',
   'ui.router',
   'project-seed.common',
   'ngAria',
   'angularMoment',
-  //'templates-app'
+  'templates-app'
 ])
-  .controller('AppCtrl', function AppCtrl($http) {
-    var app = this;
 
-    $http.get('https://api.github.com/users').then(function(result) {
-      console.log(result);
-    })
-  })
 ;
---
+++ project-seed/src/app/user-list/user-list.js
@@ -0,0 +1,19 @@
+angular.module('ps.user-list', [
+  'ui.router'
+])
+  .config(function($stateProvider) {
+    $stateProvider
+      .state('userList', {
+        url: '/users',
+        templateUrl: 'user-list/user-list.tpl.html',
+        controller: 'UserListCtrl as userList'
+      })
+  })
+  .controller('UserListCtrl', function UserListCtrl($http) {
+    var userList = this;
+
+    $http.get('https://api.github.com/users').then(function (result) {
+      userList.users = result.data;
+    })
+  })
+;
